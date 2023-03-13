6ee61b2e9c1bb2390a074f8066087be8d4a66f12
ignoramusSapien <124562204+ignoramusSapient@users.noreply.github.com>
2023-03-13 13:08:23 +0000

    Added Dynamic Data & API Route

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app=Flask(__name__)
 
+Poems = [
+    {
+          'Title': 'Chanchal Mann',
+          'Genre': 'Life'
+    },
+ {
+          'Title': 'Bachpan Bahut Sunder',
+          'Genre': 'Childhood'
+    },
+   {
+          'Title': 'Shaam Badalon Se',
+          'Genre': 'Nature'
+    },
+]
+
 @app.route('/')
 def home_func():
-  return render_template('home.html')
+  return render_template('home.html', poems=Poems)
+
+@app.route('/api/poems')
+def full_poems():
+  return jsonify(Poems)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
